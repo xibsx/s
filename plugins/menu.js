b9ce80b781bb1c564da6f2d6c2c32e301a3f0c9d
xibsx <xibstech@gmail.com>
2025-08-06 01:07:03 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -15,8 +15,8 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         let dec = ` ⤪ ⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮ 
-⤬ *SAGITTARIUSTERMINATOR* ⤬
-⤪ ⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮
+⤬ *SAGITTARIUS*      ⤬
+⤪ ⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮
 
 
 █░█ █▀▀ █░ █░ █▀█ █  
