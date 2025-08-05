33659bac815a6a5f6129e24ba8756b933ce74c67
xibsx <xibstech@gmail.com>
2025-08-05 19:24:50 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -61,13 +61,13 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 ⤪⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮ 
 ⤬ *EXPANDED MENU*    ⤬
 ⤪⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮ 
-         ⤬⤬⤬
+         
      ⤪⤬⤬⤬⤬⤬⤬⤬⤮ 
      ⤪⤬⤬⤬⤬⤬⤬⤬⤮ 
      ⤪⤬⤬⤬⤬⤬⤬⤬⤮ 
-         ⤬⤬⤬
-         ⤬⤬⤬
-         ⤩⤩⤩ 
+         
+        
+     
 ╭━〔DOWNLOAD MENU 〕━⊷
 │   
 ├➤ facebook
