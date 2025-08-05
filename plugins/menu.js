96ea8131103ac8ea54816b427ce9fd58bfdd8ee3
xibsx <xibstech@gmail.com>
2025-08-06 01:27:40 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -16,7 +16,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     try {
         let dec = ` ⤪ ⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮ 
 ⤬ *SAGITTARIUS*      ⤬
-⤪ ⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮
+⤪ ⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤬⤮
 
 
 █░█ █▀▀ █░ █░ █▀█ █  
