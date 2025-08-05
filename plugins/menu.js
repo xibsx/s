bbc1b3b7c705396e3ca390001df689ce574ae1fd
xibsx <xibstech@gmail.com>
2025-08-06 01:42:27 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,7 +9,7 @@ cmd({
     pattern: "menu",
     desc: "menu the bot",
     category: "menu",
-    react: "🐇",
+    react: "♦️",
     filename: __filename
 }, 
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
